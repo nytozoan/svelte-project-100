1df6de71e8fa2f15b27d34f7c5b9b2d457c99b3e
nytozoan <135359489+nytozoan@users.noreply.github.com>
2023-08-02 22:46:28 +0800

    Added margins and tray buttons

--- src/routes/Tray.svelte
+++ src/routes/Tray.svelte
@@ -18,8 +18,29 @@
 <div id="main">
     <ul>
         <li>Placeholders</li>
-        <li><b>B</b></li>
-        <li><i>I</i></li>
-        <li><u>U</u></li>
+        <li><button id="tray-bold" on:click={() => bold()}>B</button></li>
+        <li><button id="tray-italicize" on:click={() => italicize()}>I</button></li>
+        <li><button id="tray-underline" on:click={() => underline()}>U</button></li>
     </ul>
 </div>
+
+<script>
+    let boldState = 0
+    const bold = () => {
+        if (boldState == 0) boldState = 1
+        else if (boldState == 1) boldState = 0
+        console.log("boldState:", boldState)
+    }
+    let italicizeState = 0
+    const italicize = () => {
+        if (italicizeState == 0) italicizeState = 1
+        else if (italicizeState == 1) italicizeState = 0
+        console.log("italicizeState:", italicizeState)
+    }
+    let underlineState = 0
+    const underline = () => {
+        if (underlineState == 0) underlineState = 1
+        else if (underlineState == 1) underlineState = 0
+        console.log("underlineState:", underlineState)
+    }
+</script>
--- src/routes/Writer.svelte
+++ src/routes/Writer.svelte
@@ -1,11 +1,52 @@
 <style>
-    #main {
-        height:936px;
-        width: 612px;
+    :root {
+        --pageLength: 936px;
+        --pageWidth: 612px;
+        --rulerThickness: 20px;
+    }
+    #writer-horizontal-select-div {
+        display: flex;
+        flex-direction: row;
+    }
+    #writer-vertical-select-div-1 {
+        display: flex;
+        flex-direction: column;
+        margin-left: auto;
+    }
+    #writer-vertical-select-div-2 {
+        display: flex;
+        flex-direction: column;
         margin-left: auto;
         margin-right: auto;
+    }
+    main {
+        height:var(--pageLength);
+        width: var(--pageWidth);
         background: gray;
     }
+    #top-ruler {
+        height: var(--rulerThickness);
+        width: var(--pageWidth);
+        background: violet;
+    }
+    #side-ruler {
+        height: var(--pageLength);
+        width: var(--rulerThickness);
+        background: violet;
+    }
+    #margin-neutral-button {
+        height: var(--rulerThickness);
+        width: var(--rulerThickness);
+    }
 </style>
-
-<div id="main" contenteditable="true"></div>
+<!-- Vertical select divs are set as IDs in case I want to add a few things on the left-hand and right-hand (with a third select div) side later. -->
+<div id="writer-horizontal-select-div">
+    <div id="writer-vertical-select-div-1">
+        <button id="margin-neutral-button"></button>
+        <div id="side-ruler"></div>
+    </div>
+    <div id="writer-vertical-select-div-2">
+        <div id="top-ruler"></div>
+        <main contenteditable="true"></main>
+    </div>
+</div>
